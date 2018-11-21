c8880e25f5ad6d28ab9e35024306ba419b72ef1f
L200184134 <44856082+L200184134@users.noreply.github.com>
2018-11-21 22:17:44 +0700

    Add files via upload

---
+++ activity 1 (shell).py
@@ -0,0 +1,14 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: F:\UMS\rpl praktek algopro\praktikum 7\activity 1.py =======
+
+No | Name shape           | Formula of Area
+---|----------------------|----------------------
+1  | Triangle             | L = 0.5 * a * t
+2  | Square               | L = s ** 2
+3  | rectangle            | L = p * l
+4  | circle               | L = pi * r ** 2
+5  | parallelogram        | L = a * t
+
+>>> 
